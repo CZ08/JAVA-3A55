6b7337ec27a10d6142c456003280ae27815b5022
CZ08 <131896729+CZ08@users.noreply.github.com>
2024-11-09 19:02:15 +0100

    Commit Prosit 4

---
+++ Prosit 4/Application/src/tn/esprit/gestionzoo/entities/Animal/Animal.java
@@ -0,0 +1,51 @@
+package tn.esprit.gestionzoo.entities.Animal;
+
+public class Animal {
+    private String family;
+    private String name;
+    private int age;
+    private boolean isMammal;
+
+    public Animal(String family, String name, int age, boolean isMammal) {
+        this.family = family;
+        setName(name);
+        setAge(age);
+        this.isMammal = isMammal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Le nom de l'animal ne peut pas être vide.");
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("L'âge de l'animal ne peut pas être négatif.");
+        }
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "family='" + family + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isMammal=" + isMammal +
+                '}';
+    }
+}
+
+
+
+
