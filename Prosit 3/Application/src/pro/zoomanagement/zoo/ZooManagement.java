e75dbe172b3ec4f19ea363cb860b64a1ef524353
CZ08 <131896729+CZ08@users.noreply.github.com>
2024-11-09 19:01:43 +0100

    Commit Prosit 3

---
+++ Prosit 3/Application/src/pro/zoomanagement/zoo/ZooManagement.java
@@ -0,0 +1,39 @@
+package pro.zoomanagement.zoo;
+
+import java.util.Scanner;
+
+public class ZooManagement {
+
+    private int nbrCages = 20;
+    private String zooName = "my zoomanagement.zoo";
+
+    public static void main(String[] args) {
+        ZooManagement zoo = new ZooManagement();
+
+        zoo.displayZooInfo();
+
+        zoo.getUserInput();
+    }
+
+    public void displayZooInfo() {
+        System.out.println(zooName + " comporte " + nbrCages + " cages");
+    }
+
+    public void getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entrez le nom du zoomanagement.zoo : ");
+        zooName = scanner.nextLine();
+
+        System.out.print("Entrez le nombre de cages : ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Veuillez entrer un nombre valide.");
+            scanner.next();
+        }
+        nbrCages = scanner.nextInt();
+
+        displayZooInfo();
+
+        scanner.close();
+    }
+}
